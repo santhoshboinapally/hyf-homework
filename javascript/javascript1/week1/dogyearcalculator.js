07d9eab724e7dc38fb25d11fd9da9ca5364bfb0a
santhoshboinapally <santhoshboinapally@gmail.com>
2021-06-28 16:17:23 +0200

    dogyearcalculator.js file is created

---
+++ javascript/javascript1/week1/dogyearcalculator.js
@@ -0,0 +1,9 @@
+let dogYearOfBirth = 2015;
+let dogYearFuture = 2025;
+let shouldShowResultInDogYears = true;
+let dogYear = dogYearFuture - dogYearOfBirth;
+if (shouldShowResultInDogYears = true) {
+    console.log("your dog will be " + dogYear + " dog years old in 2025");
+} else {
+    console.log("your dog will be " + dogYear + " human years old in 2025");
+}

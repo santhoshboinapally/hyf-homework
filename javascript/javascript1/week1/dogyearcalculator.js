4fbc24710c9f4e4a622aa43d19fe2d4358096a35
santhoshboinapally <64426776+santhoshboinapally@users.noreply.github.com>
2021-07-02 16:12:18 +0200

    updated code by replacing let to const

--- javascript/javascript1/week1/dogyearcalculator.js
+++ javascript/javascript1/week1/dogyearcalculator.js
@@ -1,9 +1,9 @@
-let dogYearOfBirth = 2015;
-let dogYearFuture = 2025;
-let shouldShowResultInDogYears = true;
-let dogYear = dogYearFuture - dogYearOfBirth;
-if (shouldShowResultInDogYears = true) {
-    console.log("your dog will be " + dogYear + " dog years old in 2025");
+const dogYearOfBirth = 2015;
+const dogYearFuture = 2045;
+const shouldShowResultInDogYears = false;
+const dogYear = dogYearFuture - dogYearOfBirth;
+if (shouldShowResultInDogYears) {
+    console.log(`your dog will be ${dogYear*7} dog years old in ${dogYearFuture}`);
 } else {
-    console.log("your dog will be " + dogYear + " human years old in 2025");
+    console.log(`your dog will be ${dogYear} human years old in ${dogYearFuture}`);
 }

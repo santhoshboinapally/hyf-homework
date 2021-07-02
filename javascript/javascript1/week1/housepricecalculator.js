0308e9e53dbfee5e18db67a7cb2c2490ea89e5e4
santhoshboinapally <64426776+santhoshboinapally@users.noreply.github.com>
2021-07-02 16:10:00 +0200

    updated code by using arrays

--- javascript/javascript1/week1/housepricecalculator.js
+++ javascript/javascript1/week1/housepricecalculator.js
@@ -1,27 +1,20 @@
-let volumeInMeters;
-let housePrice;
+const gardenSizeInM2 = [100, 70];
+const width = [8, 5];
+const depth = [10, 11];
+const height = [10, 8];
 // Peters house price calculation 
-let gardenSizeInM2 = 100;
-let width = 8,
-    depth = 10,
-    height = 10;
 let actualHousePrice = 2500000;
-volumeInMeters = width * depth * height;
-housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
-
+const volumeInMeters1 = width[0] * depth[0] * height[0];
+let housePrice = volumeInMeters1 * 2.5 * 1000 + gardenSizeInM2[0] * 300;
 if (actualHousePrice > housePrice) {
     console.log("Peter is paying too much");
 } else {
     console.log("Peter is paying reasonable");
 }
 // Julia house price calculation
-gardenSizeInM2 = 70;
-width = 5;
-depth = 11;
-height = 8;
 actualHousePrice = 1000000;
-volumeInMeters = width * depth * height;
-housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+const volumeInMeters2 = width[1] * depth[1] * height[1];
+housePrice = volumeInMeters2 * 2.5 * 1000 + gardenSizeInM2 * 300;
 if (actualHousePrice > housePrice) {
     console.log("Julia is paying too much")
 } else {

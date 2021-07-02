5081725504e58dc7f022f84884a58340348597af
santhoshboinapally <64426776+santhoshboinapally@users.noreply.github.com>
2021-07-02 14:53:17 +0200

    changed let to const and updated code

--- javascript/javascript1/week1/startupname.js
+++ javascript/javascript1/week1/startupname.js
@@ -1,5 +1,6 @@
-let firstWords = ["Samsung", "Apple", "Sony", "Moto", "OnePlus", "Microsoft", "RedHat", "Ford", "Mitsubishi", "Tesla"];
-let lastWords = ["Galaxy", "IPhone", "Experia", "Razor", "Nord", "Windows", "Linux", "Fiesta", "SpaceStar", "Model S"];
-const randomNumber = Math.floor(Math.random() * 10);
-let startUpName = firstWords[randomNumber] + " " + lastWords[randomNumber];
-console.log("The startup : " + startUpName + " contains " + startUpName.length + " characters ");
+const firstWords = ["Samsung", "Apple", "Sony", "Moto", "OnePlus", "Microsoft", "RedHat", "Ford", "Mitsubishi", "Tesla"];
+const lastWords = ["Galaxy", "IPhone", "Experia", "Razor", "Nord", "Windows", "Linux", "Fiesta", "SpaceStar", "Model S"];
+const randomNumber1 = Math.floor(Math.random() * firstWords.length);
+const randomNumber2 = Math.floor(Math.random() * lastWords.length);
+const startUpName = firstWords[randomNumber1] + " " + lastWords[randomNumber2];
+console.log(`The startup ${startUpName} contains ${startUpName.length} characters `);

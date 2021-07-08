dc44a16a4383d19a69b823676a85538ca9507b6b
santhoshboinapally <santhoshboinapally@gmail.com>
2021-07-08 17:27:13 +0200

    name.js file added

---
+++ javascript/javascript1/week2/name.js
@@ -0,0 +1,21 @@
+function getFullname(firstname, surname, useFormalName) {
+    if (useFormalName) {
+        let formal = "Lord";
+        return formal + "" + firstname + "" + surname;
+    } else {
+        return firstname + surname;
+    }
+}
+const fullname1 = getFullname("Benjamin", "Hughes", true);
+const fullname2 = getFullname("Benjamin", "Hughes", false);
+console.log(fullname1);
+console.log(fullname2);
+
+/* if we have to show the name based on gender 
+function getFullname(firstname, surname, useformalname, gender) {
+    if (useformalname === true && gender === 'male') {
+        console.log("Lord" + "" + firstname + "" + surname);
+    } else { console.log('Queen' + "" + firstname + "" + surname); }
+}
+getFullname('Benjamin', 'Hughes', true, 'male');
+getFullname('Julia', 'Hughes');*/

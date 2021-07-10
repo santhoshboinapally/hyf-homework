8b24434b5ffe00bfb410e63effc44e62846c3d2e
santhoshboinapally <64426776+santhoshboinapally@users.noreply.github.com>
2021-07-10 13:46:12 +0200

    Updated as per review

--- javascript/javascript1/week2/student.js
+++ javascript/javascript1/week2/student.js
@@ -1,10 +1,9 @@
-const class07Students = ["a", "b"];
-
+const class07Students = [];
 function addStudentToClass(studentName) {
-    if (studentName === "") {
+    if (studentName.trim() === " ") {
         console.log("You cannot add an empty string to a class 07");
     } else if (studentName === 'Queen') {
-        class07Students.push(studentName);
+        class07Students.push(studentName.trim());
         console.log("Queen is added to the class 07");
     } else if (class07Students.includes(studentName)) {
         console.log("Student " + studentName + " is already in the class 07");
@@ -15,17 +14,18 @@ function addStudentToClass(studentName) {
         console.log("Student " + studentName + " is added to Class 07");
     }
 }
-
 function getNumberOfStudents() {
     return class07Students.length;
 }
-addStudentToClass("e");
-addStudentToClass("f");
 addStudentToClass("a");
 addStudentToClass("b");
 addStudentToClass("c");
 addStudentToClass("d");
+addStudentToClass("e");
+addStudentToClass("f");
 addStudentToClass("king");
+addStudentToClass("c");
 addStudentToClass("Queen");
+addStudentToClass("a");
 console.log(class07Students);
 console.log("The number of students in the class is " + getNumberOfStudents());

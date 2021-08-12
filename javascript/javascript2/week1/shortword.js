413b963cff7cee539336cb8a6c6bd09f64e3a93b
santhoshboinapally <santhoshboinapally@gmail.com>
2021-08-12 19:09:35 +0200

    shortestword.js file is added.

---
+++ javascript/javascript2/week1/shortword.js
@@ -0,0 +1,14 @@
+const danishWords = ["bil", "plante", "kaffe", "bog", "ø", "planetarium"];
+const shortWord = getShortestWord(danishWords);
+
+function getShortestWord(words) {
+    let newWord = danishWords[0];
+    for (let i = 1; i < danishWords.length; i++) {
+        if (danishWords[i].length <= newWord.length) {
+            newWord = danishWords[i];
+        }
+    }
+    return newWord;
+}
+document.getElementById("ShortestWord").innerHTML =
+    "Shortest word in array is : " + shortWord;

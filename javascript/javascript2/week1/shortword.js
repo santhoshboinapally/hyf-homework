771346f9e087e45e740d94047a1285a8a1013c0a
santhoshboinapally <santhoshboinapally@gmail.com>
2021-08-13 15:05:45 +0200

    shortword.js file is added

--- javascript/javascript2/week1/shortword.js
+++ javascript/javascript2/week1/shortword.js
@@ -1,7 +1,7 @@
 const danishWords = ["bil", "plante", "kaffe", "bog", "ø", "planetarium"];
 const shortWord = getShortestWord(danishWords);
 
-function getShortestWord(words) {
+function getShortestWord() {
     let newWord = danishWords[0];
     for (let i = 1; i < danishWords.length; i++) {
         if (danishWords[i].length <= newWord.length) {

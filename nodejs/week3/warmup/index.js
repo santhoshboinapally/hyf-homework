eb6c62e5f864f7953d425080d43809208d0d61ad
Santhosh Boinapally <64426776+santhoshboinapally@users.noreply.github.com>
2021-11-11 11:24:51 +0530

    Updated with post method

--- nodejs/week3/warmup/index.js
+++ nodejs/week3/warmup/index.js
@@ -18,6 +18,22 @@ app.get("/calculator/division", (req, res) => {
     const { first, second } = req.query;
     res.send(`<h1>Division of given numbers : ${parseInt(first) / parseInt(second)}</h1>`);
 });
+app.post("/calculator/add", (req, res) => {
+    const { first, second } = req.body;
+    res.send(`<h1>Addition of given numbers :  ${parseInt(first) + parseInt (second)}</h1>`);
+});
+app.post("/calculator/multiply", (req, res) => {
+    const { first, second } = req.body;
+    res.send(`<h1>Multiplication of given numbers : ${parseInt(first) * parseInt(second)}</h1>`);
+});
+app.post("/calculator/subtraction", (req, res) => {
+    const { first, second } = req.body;
+    res.send(`<h1>Substraction of given numbers : ${parseInt(first) - parseInt(second)}</h1>`);
+});
+app.post("/calculator/division", (req, res) => {
+    const { first, second } = req.body;
+    res.send(`<h1>Division of given numbers : ${parseInt(first) / parseInt(second)}</h1>`);
+});
 
 
 app.listen(3000, () => console.log(`Calculator:listening on port 3000`));

251d90c32eec3d327e16d43b4757d5f475df241a
Santhosh Boinapally <64426776+santhoshboinapally@users.noreply.github.com>
2021-12-01 21:18:03 +0100

    updated timer.js

--- react/week4/github-user-search/src/components/timer.js
+++ react/week4/github-user-search/src/components/timer.js
@@ -1,10 +1,11 @@
 import  React, { useState , useEffect } from 'react'
 export const DateTime = () => {
-    var [date,setDate] = useState(new Date());
+    let [date,setDate] = useState(new Date());
         useEffect(() => {
-        var timer = setInterval(()=>setDate(new Date()), 1000 )
+        let timer = setInterval(()=>setDate(new Date()), 1000 )
         return function cleanup() {
             clearInterval(timer)
+            
         }
         });
     return(
